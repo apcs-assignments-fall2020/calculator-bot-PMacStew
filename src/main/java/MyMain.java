30905e99e092f96321e52ff854e7db5c375f5f08
PMacStew <23pmcclure@athenian.org>
2020-09-04 03:47:20 +0000

    finish homework

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,29 +2,71 @@ import java.util.Scanner;
 
 public class MyMain {
 
-    // Calculates the median of the three inputs
-    public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+    // Calculates edian of the three inputs
+    public static int median(final int a, final int b, final int c) {
+        if (Math.max(a, b) == a) {
+            if (Math.max(a, c) == a)
+                return Math.max(b, c);
+            else
+                return a;
+        }
+        else {
+            if (Math.max(b, c) == b)
+                return Math.max(c, a);
+            else
+                return b;
+        }
+    
+
     }
 
     // Returns the input with the larger absolute value
-    public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+    public static int magnitude(final int a, final int b) {
+        final int x = Math.abs(a);
+        final int y = Math.abs(b);
+        if (Math.max(x, y) == x)
+            return a;
+        else
+            return b;
     }
 
     // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
     // where "a" and "b" are the inputs to the method
-    public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+    public static double pythagoras(final int a, final int b) {
+        return Math.sqrt((a * a) + (b * b));
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // You may want to keep these lines of code to test that your methods work
-        System.out.println(median(1, 2, 3)); // should be 2
-        System.out.println(magnitude(-7, -1)); // should be -7
-        System.out.println(pythagoras(3, 4)); // should be 5.0
+        final Scanner scan = new Scanner(System.in);
+        System.out.println("Would you like to 1. find the median of 3 numbers 2. find the larger absolute value of two numbers or 3. find the hypotenuse of a triangle using the pythagorean theorem?");
+        System.out.print("Please enter the corresponding digit: ");
+        final int methodChoice = scan.nextInt();
+        if (methodChoice == 1) {
+            System.out.print("Input first number: ");
+            final int median1 = scan.nextInt();
+            System.out.print("Input second number: ");
+            final int median2 = scan.nextInt();
+            System.out.print("Input third number: ");
+            final int median3 = scan.nextInt();
+            System.out.println("The median of your numbers is " + median(median1, median2, median3));
+        }
+        else if (methodChoice == 2){
+            System.out.print("Input first number: ");
+            final int magnitude1 = scan.nextInt();
+            System.out.print("Input second number ");
+            final int magnitude2 = scan.nextInt();
+            System.out.println("The largest absolute value of your two numbers is " + magnitude(magnitude1, magnitude2));
+        }
+        else {
+            System.out.print("Input length of first side: ");
+            final int pythagorean1 = scan.nextInt();
+            System.out.print("Input length of second side: ");
+            final int pythagorean2 = scan.nextInt();
+            System.out.println("The length of your hypotenuse is " + pythagoras(pythagorean1, pythagorean2));
+        }
 
-        Scanner scan = new Scanner(System.in);
+        
         // YOUR CODE HERE
     }
 }
